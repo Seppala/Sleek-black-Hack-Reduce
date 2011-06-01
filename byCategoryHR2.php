af861146deb4f43bff1a4c8cac53b9dbf528d216
Riku <riku@seppa.la>
2011-06-01 15:00:23 -0400

    Trying to get the header to work on the event page

--- byCategoryHR2.php
+++ byCategoryHR2.php
@@ -46,7 +46,7 @@ Template Name: ListPostsInCategoryThatHasSameNameAsPage_TOC
 			<?php the_content(); ?>
 		<?php endwhile; else: endif; ?>
 			
-		<h2 class="fromthe">From the blog about <?php str_replace("»", "", wp_title(); ?></h2>	
+		<h2 class="fromthe">From the blog about <?php str_replace("»", "", wp_title()); ?></h2>	
 		<?php query_posts ('category_name='.get_the_title().'&post_status=publish,future'); ?>
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?> 
 					
