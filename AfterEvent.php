dd36dfa8fdf56a34635926bb763d788eeba54d15
Riku <riku@seppa.la>
2011-07-05 17:11:09 -0400

    Changes to AfterEvent, still not working

--- AfterEvent.php
+++ AfterEvent.php
@@ -65,6 +65,10 @@ Template Name: ListPostsInCategoryThatHasSameNameAsPage_TOC
 		<p><?php the_content(); ?>
 		<?php endwhile; else: endif; ?>
 
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+			<?php endwhile; else: endif; ?>
 
 <div class="separator"></div>
 
